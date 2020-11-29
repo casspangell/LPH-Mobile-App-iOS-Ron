325b8bff205b6148c36db1d2430b696f07e7b7e5
Cass Pangell <projectxcappe@Casss-MacBook-Pro.local>
2020-11-29 14:13:44 -0700

    Added RoundedCornerView.swift.

--- LovePeaceHarmony/Modules/Utils/LPHUtils.swift
+++ LovePeaceHarmony/Modules/Utils/LPHUtils.swift
@@ -209,6 +209,32 @@ extension UIColor {
     }
 }
 
+@IBDesignable
+class RoundedCornerView: UIView {
+    
+    // if cornerRadius variable is set/changed, change the corner radius of the UIView
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+}
+
+
 extension MutableCollection {
     /// Shuffles the contents of this collection.
     mutating func shuffle() {
