489afa641ab729da39516b38599e797c36af2a8f
Cass Pangell <projectxcappe@Casss-MacBook-Pro.local>
2021-01-03 13:27:05 -0700

    Deleted commented out unnecessary code.

--- LovePeaceHarmony/Modules/Controller/Chant/ChantMilestoneController.swift
+++ LovePeaceHarmony/Modules/Controller/Chant/ChantMilestoneController.swift
@@ -82,11 +82,6 @@ class ChantMilestoneController: BaseViewController, IndicatorInfoProvider {
         initiateShare()
     }
     
-    // MARK: - Actions
-//    private func renderUI(isLoginViewShown: Bool) {
-//        viewMilestoneContainer.isHidden = isLoginViewShown
-//    }
-    
     private func populateData(milestoneVo: MilestoneVo) {
         LPHUtils.setUserDefaultsFloat(key: UserDefaults.Keys.chantDay, value: Float(milestoneVo.daysCount)!)
         LPHUtils.setUserDefaultsFloat(key: UserDefaults.Keys.chantMinute, value: Float(milestoneVo.minutesCount)!)
@@ -139,54 +134,7 @@ class ChantMilestoneController: BaseViewController, IndicatorInfoProvider {
         }
         
     }
-    
-//    private func initiateLogin(type: LoginType) {
-//        do {
-//            try loginEngine?.initiateLogin(type) { (lphResponse) in
-//                if lphResponse.isSuccess() {
-//                    let loginVo = lphResponse.getResult()
-//
-//                    InstanceID.instanceID().instanceID { (result, error) in
-//                    if let error = error {
-//                    print("Error fetching remote instange ID: \(error)")
-//                    } else if let result = result {
-//                    print("Remote instance ID token: \(result.token)")
-//                        self.fireSocialLoginRegisterApi(email: loginVo.email, password: loginVo.password, name: loginVo.fullName, profilePic: loginVo.profilePicUrl, source: type, deviceId: result.token)
-//                     }
-//                    }
-//
-//                } else {
-//
-//                }
-//            }
-//        } catch let exception as LPHException<LoginError> {
-//
-//        } catch {
-//
-//        }
-//    }
-    
-//    private func processLoginResponse(source loginType: LoginType, password: String, serverResponse response: LPHResponse<ProfileVo, LoginError>) {
-//        if response.isSuccess() {
-//
-//            let profileVo = response.getResult()
-//            let loginVo = LPHUtils.getLoginVo()
-//            loginVo.isLoggedIn = true
-//            loginVo.email = profileVo.email
-//            loginVo.password = password
-//            loginVo.fullName = profileVo.name
-//            loginVo.profilePicUrl = profileVo.profilePic
-//            loginVo.loginType = loginType
-//            loginVo.inviteCode = profileVo.inviteCode
-//            loginVo.token = response.getMetadata() as! String
-//            LPHUtils.setLoginVo(loginVo: loginVo)
-//            LPHUtils.setUserDefaultsBool(key: UserDefaults.Keys.mandarinSoulEnglish, value: true)
-//            LPHUtils.setUserDefaultsBool(key: UserDefaults.Keys.isInstrumentalOn, value: true)
-//            LPHUtils.setUserDefaultsBool(key: UserDefaults.Keys.isHindiOn, value: true)
-//            fireUpdateTokenApi()
-//        }
-//    }
-    
+
     // MARK: - XLPagerTabStrip
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         return IndicatorInfo(title: "Title")
@@ -226,60 +174,4 @@ class ChantMilestoneController: BaseViewController, IndicatorInfoProvider {
             
         }
     }
-    
-//    private func fireSocialLoginRegisterApi(email: String,password: String, name: String, profilePic: String, source: LoginType, deviceId: String) {
-//        showLoadingIndicator()
-//        do {
-//            let lphService: LPHService = try LPHServiceFactory<LoginError>.getLPHService()
-//            lphService.fireLoginRegister(email: email, password: password, name: name, profilePicUrl: profilePic, source: source, deviceId: deviceId) { (lphResponse) in
-//                if lphResponse.isSuccess() {
-//                    self.processLoginResponse(source: source, password: password, serverResponse: lphResponse)
-//                }
-//                self.hideLoadingIndicator()
-//            }
-//        } catch let error {
-//
-//        }
-//    }
-    
-//    private func fireUpdateTokenApi() {
-//        var deviceToken = String()
-//        let deviceInfo = DEVICE_INFO
-//        showLoadingIndicator()
-//
-//        InstanceID.instanceID().instanceID { (result, error) in
-//        if let error = error {
-//        print("Error fetching remote instange ID: \(error)")
-//        } else if let result = result {
-//        print("Remote instance ID token: \(result.token)")
-//            deviceToken = result.token
-//         }
-//        }
-//
-//        do {
-//            let lphService = try LPHServiceFactory<LoginError>.getLPHService()
-//            try lphService.updateDeviceToken(token: deviceToken, info: deviceInfo) { (parsedResponse) in
-//                self.hideLoadingIndicator()
-//                let loginVo = LPHUtils.getLoginVo()
-//                if loginVo.isLoggedIn && loginVo.loginType == .withoutLogin {
-//                    self.renderUI(isLoginViewShown: true)
-//                } else {
-//                    if self.loginType != loginVo.loginType {
-//                        self.fireMilestoneApi()
-//                    } else {
-//                        self.renderUI(isLoginViewShown: false)
-//                    }
-//                }
-//                self.loginType = loginVo.loginType
-//            }
-//        } catch let error {
-//
-//        }
-//    }
-    
-//    private func fireMilestoneApi() {
-//        renderUI(isLoginViewShown: false)
-//        fireMilestoneDetails()
-//    }
 
-}
